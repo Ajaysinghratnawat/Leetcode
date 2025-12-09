cef6e25505ebd8a945b6aca8cdeab2dff6a978bb
Ajay singh ratnawat <168153835+Ajaysinghratnawat@users.noreply.github.com>
2025-12-09 12:36:20 +0530

    Time: 1 ms (32.24%) | Memory: 43 MB (37.91%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -3,5 +3,16 @@ class Solution {
         String arr[] = s.split(" ");
         String s2 = arr[arr.length-1];
         return s2.length();
+        // int count = 0;
+        // for(int i=s.length()-1;i>=0;i--){
+        //     char ch = s.charAt(i);
+        //     if(ch==' '){
+        //         break;
+        //     }
+        //     else{
+        //         count++;
+        //     }
+        // }
+        // return count;
     }
 }
