9576bc336207565a0a692e7dd3e2d77604c12781
Ajay singh ratnawat <168153835+Ajaysinghratnawat@users.noreply.github.com>
2025-12-11 16:39:46 +0530

    Time: 4 ms (37.96%) | Memory: 43.1 MB (46.28%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,69 @@
+// class Solution {
+//     public boolean isValid(String s) {
+//         Stack<Character> st = new Stack<>();
+//         for(int i=0;i<s.length();i++){
+//             if(st.isEmpty()) {
+// 				if(s.charAt(i)==')'){
+//                     if(!st.isEmpty()&&st.peek()=='('){
+//                     char t = st.pop();
+//                     }
+//                     else{
+//                         return false;
+//                     }
+//                 }
+// 				if(s.charAt(i)=='}'){
+//                     if(!st.isEmpty()&&st.peek()=='{'){
+//                         char t = st.pop();
+//                     }
+//                     else{
+//                         return false;
+//                     }
+//                 }
+//                 if(s.charAt(i)==']'){
+//                     if(!st.isEmpty()&&st.peek()=='['){
+//                         char t = st.pop();
+//                     }
+//                     else{
+//                         return false;
+//                     }
+//                 }
+
+// 			}
+//             if(s.charAt(i)=='('||s.charAt(i)=='{'||s.charAt(i)==']'){
+// 			    st.push(s.charAt(i));
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        HashMap<Character,Character> mp = new HashMap<>();
+        mp.put(')','(');
+        mp.put('}','{');
+        mp.put(']','[');
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch=='('||ch=='{'||ch=='['){
+                st.push(ch);
+            }
+            else{
+                char cb=mp.get(ch);
+                if(!st.isEmpty()&&cb==st.peek()){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        if(st.size()>0){
+            return false;
+        }
+        return true;
+    }
+}
