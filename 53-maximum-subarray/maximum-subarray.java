0e70e55bf24910ad1b8085e48fa77ee0b9c44c90
Ajay singh ratnawat <168153835+Ajaysinghratnawat@users.noreply.github.com>
2025-12-13 17:05:28 +0530

    Time: 1 ms (99.76%) | Memory: 78.8 MB (9.72%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,13 +1,18 @@
 class Solution {
     public int maxSubArray(int[] nums) {
+        //to store sum 
         int ans = Integer.MIN_VALUE;
 		int n = nums.length;
+        //prefix array
 		int[] p = new int[n];
+        //prefix sum logic
 		p[0] = nums[0];
 		for(int i=1;i<n;i++) {
 			p[i]=p[i-1]+nums[i];
 		}
+        //sirf negative check karna hai
 		int min=0;
+        //for check the sum of sub array
 		for(int i=0;i<n;i++) {
 			int curr = p[i];
 			ans = Math.max(ans, curr-min);
