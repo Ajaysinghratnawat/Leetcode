3eb8e3da004f3e2919ac8435d77495acd36961a1
Ajay singh ratnawat <168153835+Ajaysinghratnawat@users.noreply.github.com>
2025-12-14 19:23:58 +0530

    Time: 1 ms (99.84%) | Memory: 55.9 MB (7.61%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -2,6 +2,7 @@ class Solution {
     public int majorityElement(int[] nums) {
         // Arrays.sort(nums);
         // return nums[nums.length/2];
+        //Boyle moore voting algo
         int count=0;
         int ans=nums[0];
         for(int i=0;i<nums.length;i++){
