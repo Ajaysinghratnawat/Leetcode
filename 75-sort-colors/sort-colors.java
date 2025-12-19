ee5c6df0f60bd1dae92083bcec68e5e063d0779b
Ajay singh ratnawat <168153835+Ajaysinghratnawat@users.noreply.github.com>
2025-12-19 15:15:24 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (58.07%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,54 +1,61 @@
 class Solution {
     public void sortColors(int[] nums) {
-        // int i=0;
-        // int j=nums.length-1;
-        // for(int k=0;k<=j;){
-        //     if(nums[k]==0){
-        //         int temp =nums[k];
-        //         nums[i]=nums[k];
-        //         nums[k]=temp;
-        //         i++;
-        //         k++;
+        // int x=0;
+        // int y=0;
+        // int z=0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]==0){
+        //         x++;
         //     }
-        //     else if(nums[k]==2){
-        //         int temp =nums[k];
-        //         nums[j]=nums[k];
-        //         nums[k]=temp;
-        //         j--;
+        //     else if(nums[i]==1){
+        //         y++;
         //     }
-        //     else{
-        //         k++;
+        //     else if(nums[i]==2){
+        //         z++;
+        //     }
+        // }
+        // for(int i=0;i<nums.length;i++){
+        //     if(x>0){
+        //         nums[i]=0;
+        //         x--;
+        //     }
+        //     else if(y>0){
+        //         nums[i]=1;
+        //         y--;
+        //     }
+        //     else if(z>0){
+        //         nums[i]=2;
+        //         z--;
         //     }
         // }
 
 
-        int x=0;
-        int y=0;
-        int z=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==0){
-                x++;
+
+
+
+
+        
+        int i=0;
+        int j=nums.length-1;
+        for(int k=0;k<=j;){
+            if(nums[k]==0){
+                int temp =nums[k];
+                nums[k]=nums[i];
+                nums[i]=temp;
+                i++;
+                k++;
             }
-            else if(nums[i]==1){
-                y++;
+            else if(nums[k]==2){
+                int temp =nums[k];
+                nums[k]=nums[j];
+                nums[j]=temp;
+                j--;
             }
-            else if(nums[i]==2){
-                z++;
-            }
-        }
-        for(int i=0;i<nums.length;i++){
-            if(x>0){
-                nums[i]=0;
-                x--;
-            }
-            else if(y>0){
-                nums[i]=1;
-                y--;
-            }
-            else if(z>0){
-                nums[i]=2;
-                z--;
+            else{
+                k++;
             }
         }
+
+
     }
 }
