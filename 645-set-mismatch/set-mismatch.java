31f747f64606af963543b7eccd98e92459c5104b
Ajay singh ratnawat <168153835+Ajaysinghratnawat@users.noreply.github.com>
2025-12-27 09:46:44 +0530

    Time: 15 ms (23.74%) | Memory: 48.7 MB (6.06%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int Actual = n * (n + 1) / 2;
+        int duplicate = 0;
+        int sum = 0;
+        int ans[] = new int[2];
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            if (i < n - 1 && nums[i] == nums[i + 1]) {
+                duplicate = nums[i + 1];
+            }
+        }
+        int Unique = sum - duplicate;
+        int res = Actual - Unique;
+        ans[0] = duplicate;
+        ans[1] = res;
+        return ans;
+    }
+}
