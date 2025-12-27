eedef637f9bd9976fd774ebd04bbb83420a7e148
Ajay singh ratnawat <168153835+Ajaysinghratnawat@users.noreply.github.com>
2025-12-27 09:51:50 +0530

    Time: 15 ms (23.74%) | Memory: 48.6 MB (6.06%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,44 +1,21 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int[] res = new int[2];
-        TreeSet<Integer> set = new TreeSet<>();
-        for (int k : nums) {
-            if (!set.contains(k)) {
-                set.add(k);
-            } else {
-                res[0] = k;
+        Arrays.sort(nums);
+        int n = nums.length;
+        int Actual = n * (n + 1) / 2;
+        int duplicate = 0;
+        int sum = 0;
+        int ans[] = new int[2];
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            if (i < n - 1 && nums[i] == nums[i + 1]) {
+                duplicate = nums[i + 1];
             }
         }
-        int j = 1;
-        for (int i = 0; i < nums.length; i++) {
-            if (!set.contains(j)) {
-                res[1] = j;
-                break;
-            }
-            j++;
-        }
-        return res;
+        int Unique = sum - duplicate;
+        int res = Actual - Unique;
+        ans[0] = duplicate;
+        ans[1] = res;
+        return ans;
     }
 }
-
-// class Solution {
-//     public int[] findErrorNums(int[] nums) {
-//         Arrays.sort(nums);
-//         int n = nums.length;
-//         int Actual = n * (n + 1) / 2;
-//         int duplicate = 0;
-//         int sum = 0;
-//         int ans[] = new int[2];
-//         for (int i = 0; i < n; i++) {
-//             sum += nums[i];
-//             if (i < n - 1 && nums[i] == nums[i + 1]) {
-//                 duplicate = nums[i + 1];
-//             }
-//         }
-//         int Unique = sum - duplicate;
-//         int res = Actual - Unique;
-//         ans[0] = duplicate;
-//         ans[1] = res;
-//         return ans;
-//     }
-// }
