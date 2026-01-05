f16083c0858e7830d67f28d2bbe83e77406b4ed6
Ajay singh ratnawat <168153835+Ajaysinghratnawat@users.noreply.github.com>
2026-01-05 17:37:42 +0530

    Time: 1 ms (99.19%) | Memory: 45.6 MB (40.52%) - LeetSync

---
+++ 419-battleships-in-a-board/battleships-in-a-board.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int countBattleships(char[][] board) {
+        // int count=0;
+        // for(int i=0;i<board.length;i++){
+        //     for(int j=0;j<board[0].length;j++){
+        //         int k=i;
+        //         int v=j;
+        //         if(board[i][j]=='X'){
+        //             if(v==j){
+        //                 count++;
+        //                 break;
+        //             }
+        //             // else if(k==i){
+        //             //     count++;
+        //             //     break;
+        //             // }
+        //             System.out.println(i+" "+j);
+        //         }
+        //     }
+        // }
+        // return count;
+
+        int count=0;
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j]=='X'){
+                    count++;
+                    board[i][j]='.';
+                    int temp=j;
+                    while(temp+1<board[0].length&&board[i][temp+1]=='X'){
+                        board[i][temp+1]='.';
+                        temp++;
+                    }
+                    int temp2=i;
+                    while(temp2+1<board.length&&board[temp2+1][j]=='X'){
+                        board[temp2+1][j]='.';
+                        temp2++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
