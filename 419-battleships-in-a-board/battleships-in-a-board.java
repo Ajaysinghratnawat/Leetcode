309ec9c105516e8f553c157575e6df8c014420c4
Ajay singh ratnawat <168153835+Ajaysinghratnawat@users.noreply.github.com>
2026-01-05 17:47:50 +0530

    Time: 1 ms (99.19%) | Memory: 45.6 MB (40.52%) - LeetSync

--- 419-battleships-in-a-board/battleships-in-a-board.java
+++ 419-battleships-in-a-board/battleships-in-a-board.java
@@ -20,21 +20,37 @@ class Solution {
         // }
         // return count;
 
+        // int count=0;
+        // for(int i=0;i<board.length;i++){
+        //     for(int j=0;j<board[0].length;j++){
+        //         if(board[i][j]=='X'){
+        //             count++;
+        //             board[i][j]='.';
+        //             int temp=j;
+        //             while(temp+1<board[0].length&&board[i][temp+1]=='X'){
+        //                 board[i][temp+1]='.';
+        //                 temp++;
+        //             }
+        //             int temp2=i;
+        //             while(temp2+1<board.length&&board[temp2+1][j]=='X'){
+        //                 board[temp2+1][j]='.';
+        //                 temp2++;
+        //             }
+        //         }
+        //     }
+        // }
+        // return count;
+
         int count=0;
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
                 if(board[i][j]=='X'){
                     count++;
-                    board[i][j]='.';
-                    int temp=j;
-                    while(temp+1<board[0].length&&board[i][temp+1]=='X'){
-                        board[i][temp+1]='.';
-                        temp++;
+                    if(i>0&&board[i-1][j]=='X'){
+                        count--;
                     }
-                    int temp2=i;
-                    while(temp2+1<board.length&&board[temp2+1][j]=='X'){
-                        board[temp2+1][j]='.';
-                        temp2++;
+                    else if(j>0&&board[i][j-1]=='X'){
+                        count--;
                     }
                 }
             }
