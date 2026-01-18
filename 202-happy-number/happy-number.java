5b5fed1c75189ab27700a8c6da622cfe932f3ab2
Ajay singh ratnawat <168153835+Ajaysinghratnawat@users.noreply.github.com>
2026-01-18 14:35:57 +0530

    Time: 1 ms (77.43%) | Memory: 42.8 MB (7.97%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isHappy(int n) {
+        HashMap<Integer, Boolean> map = new HashMap<>();
+
+        while (n != 1) {
+            if (map.containsKey(n)) {
+                return false;
+            }
+            map.put(n, true);
+
+            int sum = 0;
+            while (n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+
+            n = sum;
+        }
+        return true;
+    }
+}
