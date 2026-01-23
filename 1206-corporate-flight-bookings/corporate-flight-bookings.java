6cfc9d285558101fba0cc2c20838784ac0d43aa9
Ajay singh ratnawat <168153835+Ajaysinghratnawat@users.noreply.github.com>
2026-01-23 17:36:30 +0530

    Time: 3 ms (99.25%) | Memory: 85 MB (12.32%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.java
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.java
@@ -3,13 +3,24 @@ class Solution {
         int[] ans=new int[n];
         for(int i=0;i<bookings.length;i++){
             int start=bookings[i][0];
-            int end = bookings[i][1];
+            int end=bookings[i][1];
             int put=bookings[i][2];
-            while(start<=end){
-                ans[start-1]=put+ans[start-1];
-                start++;
-            }
+            ans[start-1]=ans[start-1]+put;
+            if(end<n) ans[end]=ans[end]-put;
         }
+        for(int i=1;i<ans.length;i++){
+            ans[i]=ans[i]+ans[i-1];
+        }
+
+        // for(int i=0;i<bookings.length;i++){
+        //     int start=bookings[i][0];
+        //     int end = bookings[i][1];
+        //     int put=bookings[i][2];
+        //     while(start<=end){
+        //         ans[start-1]=put+ans[start-1];
+        //         start++;
+        //     }
+        // }
         return ans;
     }
 }
