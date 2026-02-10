ecbd4e83061e753da01812ea162bacb858b0df6e
Ajay singh ratnawat <168153835+Ajaysinghratnawat@users.noreply.github.com>
2026-02-10 15:09:05 +0530

    Update longest-balanced-subarray-i.java
    Refactor longestBalanced method to improve readability and functionality.

--- 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.java
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.java
@@ -1,36 +1,3 @@
-// class Solution {
-//     public int longestBalanced(int[] nums) {
-//         int ans = nums.length;
-//         int even=0;
-//         int odd=0;
-//         TreeSet<Integer> t = new TreeSet<>();
-//         for(int i=0;i<nums.length;i++){
-//             t.add(nums[i]);
-//         }
-//         for(int i:t){
-//             System.out.println(i);
-//         }
-//         for(int i:t){
-//             if(i%2==0){
-//                 even++;
-//             }
-//             else{
-//                 odd++;
-//             }
-//         }
-//         if(even==odd) return ans;
-//         else if(even<odd){
-//             return even;
-//         }
-//         else if(even>odd){
-//             return odd;
-//         }
-//         if(ans==2 && even!=odd) return 0;
-//         return --ans;
-//     }
-// }
-
-
 class Solution {
     public int longestBalanced(int[] nums) {
         int max = 0;
