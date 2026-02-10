1fad7e3d3a31211be37b21a56a5c4f8a30ee05cc
Ajay singh ratnawat <168153835+Ajaysinghratnawat@users.noreply.github.com>
2026-02-10 16:30:30 +0530

    Time: 5 ms (59.46%) | Memory: 45.2 MB (33.74%) - LeetSync

--- 71-simplify-path/simplify-path.java
+++ 71-simplify-path/simplify-path.java
@@ -1,16 +1,16 @@
 class Solution {
     public String simplifyPath(String path) {
         Stack<String> stack = new Stack<>();
-        String[] directories = path.split("/");
-        for (String dir : directories) {
-            if (dir.equals(".") || dir.isEmpty()) {
+        String[] temp = path.split("/");
+        for (String i : temp) {
+            if (i.equals(".") || i.isEmpty()) {
                 continue;
-            } else if (dir.equals("..")) {
+            } else if (i.equals("..")) {
                 if (!stack.isEmpty()) { 
                     stack.pop();
                 }
             } else { 
-                stack.push(dir);
+                stack.push(i);
             }
         }
         return "/" + String.join("/", stack);
